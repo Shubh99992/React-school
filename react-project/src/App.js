25c923302e350966d8a2b95c6be1a342a40ffa5c
Shubham <105529358+Shubh99992@users.noreply.github.com>
2024-05-06 05:03:16 +0000

    Menu bar day 6

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import Menubar from './component/Menubar'
 import Home from './component/test'
-import Weather from './component/Weather'
-import {browserRouter as Router, Route, Switch} from 'react-router-dom'
+import Weather from './component/weather'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <h2>Welcome to React</h2>
-      <browserRouter>
+      <BrowserRouter>
       <Menubar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Product" element={<Weather />} />
+        <Route path="/Weather" element={<Weather />} />
       </Routes>
 
-      </browserRouter>
+      </BrowserRouter>
     </div>
   )
 }
--- react-project/src/component/Menubar.js
+++ react-project/src/component/Menubar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'react-router-dom'
+import {Link }from 'react-router-dom'
 
 const Menubar = () => {
   return (
@@ -9,7 +9,7 @@ const Menubar = () => {
                 <Link to="/">Home</Link>
             </div>
             <div>
-                <Link to="/Product">Home</Link>
+                <Link to="/Weather">Weather</Link>
             </div>
         </nav>
     </div>
